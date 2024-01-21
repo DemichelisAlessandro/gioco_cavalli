b2cc772b6261847f68c253ee3ac68f7ab05694c1
ElAmrani-Firas <120721984+ElAmrani-Firas@users.noreply.github.com>
2024-01-21 18:40:06 +0100

    Parte iniziale del progetto

---
+++ script.js
@@ -0,0 +1,50 @@
+function onload(){
+    let rand = 0;
+    let cont = 0;
+    let trovato = 0;
+    do {
+        rand = Math.round(Math.random() * 48) + 1;
+        for(let i; i<vettore.length; i++){
+            if(vettore[i] == rand){
+                trovato = 1;
+            }
+        }
+        if(trovato == 0){
+            vettore.push(rand);
+            cont++;
+            if(cont==1){
+                div1.src(`Immagini Carte/`+ rand +`.gif`);
+            }
+            if(cont==2){
+                div2.src(`Immagini Carte/`+ rand +`.gif`);
+            }
+            if(cont==3){
+                div3.src(`Immagini Carte/`+ rand +`.gif`);
+            }
+            if(cont==4){
+                div4.src(`Immagini Carte/`+ rand +`.gif`);
+            }
+            if(cont==5){
+                div5.src(`Immagini Carte/`+ rand +`.gif`);
+            }
+            if(cont==6){
+                div6.src(`Immagini Carte/`+ rand +`.gif`);
+            }
+        }
+
+        
+    } while (cont <= 6);
+}
+
+
+var div1 = document.getElementById("1");
+var div2 = document.getElementById("2");
+var div3 = document.getElementById("3");
+var div4 = document.getElementById("4");
+var div5 = document.getElementById("5");
+var div6 = document.getElementById("6");
+var div7 = document.getElementById("7");
+
+
+
+var vettore = [];

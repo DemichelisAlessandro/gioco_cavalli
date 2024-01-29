fd5d05793581857f26a528d07abf928009b3f6d8
coding like a pro <mummiamummia735@gmail.com>
2024-01-29 23:12:13 +0100

    praticamente finito
    l'unica cosa che potremmo aggiungere è che una volta finita la partita si può ricominciare da zero, per fare ciò basta creare un pulsante che richiama in qualche modo l'index.html penso che si possa fare il il tag <a></a>(quello dei link), ovviamente questo sito ha un aspetto orrendo considerando che il css ha tipo 5 righe di codice e non è neanche tanto bello il codice visto che non è per niente ottimizzato ma almeno funziona

--- poi sistemo/Esercizio05/registrazione.php
+++ poi sistemo/Esercizio05/registrazione.php
@@ -23,23 +23,25 @@
 
     <?php
 
-    $ripetuto = true;
-    $vet = array();
-    $vet2 = array();
-    $vittoria = "";
-    $scommessa = "";
+    $ripetuto = true;//serve a controllare se la carta è già stata pescata quando pesco una nuova carta
+    $vuoto = true;//serve quando una riga si svuota quindi devo pescare 2 carte ma per visualizzare entrambe le carte mi serve una if con questa variabile
+    $vet = array();//contiene tutte le carte pescate finora per evitare ripetizioni
+    $vet2 = array();//contiene la posizione dei vari semi nella tabella
+    $vet3 = array();//contiene le carte a lato della tabella che vengono pescate quando non ce più nessuno nella righa
+    $vittoria = "";//contiene il vincitore
+    $scommessa = "";//contiene su chi si scommette
 
-    if ($_GET["sco"] != null) {
+    if ($_GET["sco"] != null) {//questa roba recupera il valore della scommessa
         $scommessa = $_GET["sco"];
     } else {
         $scommessa = "picche";
     }
 
-    if ($_GET["vin"] != null) {
+    if ($_GET["vin"] != null) {//questa riba recupera il valore di vittoria e se è vuoto contina il gioco
         $vittoria = $_GET["vin"];
     } else {
         $vittoria = "";
-        if ($_GET["veto"] != null) {
+        if ($_GET["veto"] != null) {//questa roba recupera il valore di vet
             $stringa = $_GET["veto"];
         } else {
             $stringa = "0";
@@ -47,7 +49,15 @@
 
         $vet = explode(',', $stringa);
 
-        if ($_GET["txtNominativo"] != null) {
+        if ($_GET["bruh"] != null) {//questa roba recupera il valore di vet3
+            $stringa = $_GET["bruh"];
+        } else {
+            $stringa = "0,0";
+        }
+
+        $vet3 = explode(',', $stringa);
+
+        if ($_GET["txtNominativo"] != null) {//questa roba recupera il valore di  vet2
             $stringa = $_GET["txtNominativo"];
         } else {
             $stringa = "0,0,0,0";
@@ -55,7 +65,7 @@
 
         $vet2 = explode(',', $stringa);
 
-        while (count($vet) < 48 && $ripetuto) {
+        while (count($vet) < 48 && $ripetuto) {//questa roba pesca una carta
             $ripetuto = false;
             $numeroCasuale = random_int(1, 48);
             for ($i = 0; $i < count($vet); $i++) {
@@ -65,7 +75,7 @@
             }
         }
         $vet[count($vet)] = $numeroCasuale;
-        if ($numeroCasuale < 13) {
+        if ($numeroCasuale < 13) {//questa roba aggiorna la posizione dei re in vet2
             $vet2[0] += 1;
         } else if ($numeroCasuale > 12 && $numeroCasuale < 25) {
             $vet2[1] += 1;
@@ -75,30 +85,84 @@
             $vet2[3] += 1;
         }
 
+        if($vet2[0] > 0 && $vet2[1] > 0 && $vet2[2] > 0 && $vet2[3] > 0 && $vet3[0] == 0){//queste enormi if e else if servono per quando una riga si svuota e bisogna quindi pesacre un'altra carta e aggiornare le varie variabili e vettori
+
+            $vuoto = false;
+
+            $ripetuto = true;
+            while (count($vet) < 48 && $ripetuto) {
+                $ripetuto = false;
+                $numeroCasuale = random_int(1, 48);
+                for ($i = 0; $i < count($vet); $i++) {
+                    if ($vet[$i] == $numeroCasuale) {
+                        $ripetuto = true;
+                    }
+                }
+            }
+            $vet[count($vet)] = $numeroCasuale;
+            if ($numeroCasuale < 13) {
+                $vet2[0] += 1;
+            } else if ($numeroCasuale > 12 && $numeroCasuale < 25) {
+                $vet2[1] += 1;
+            } else if ($numeroCasuale > 24 && $numeroCasuale < 37) {
+                $vet2[2] += 1;
+            } else {
+                $vet2[3] += 1;
+            }
+            $vet3[0] = $numeroCasuale;
+
+        }else if ($vet2[0] > 1 && $vet2[1] > 1 && $vet2[2] > 1 && $vet2[3] > 1 && $vet3[1] == 0){
+
+            $vuoto = false;
+
+            $ripetuto = true;
+            while (count($vet) < 48 && $ripetuto) {
+                $ripetuto = false;
+                $numeroCasuale = random_int(1, 48);
+                for ($i = 0; $i < count($vet); $i++) {
+                    if ($vet[$i] == $numeroCasuale) {
+                        $ripetuto = true;
+                    }
+                }
+            }
+            $vet[count($vet)] = $numeroCasuale;
+            if ($numeroCasuale < 13) {
+                $vet2[0] += 1;
+            } else if ($numeroCasuale > 12 && $numeroCasuale < 25) {
+                $vet2[1] += 1;
+            } else if ($numeroCasuale > 24 && $numeroCasuale < 37) {
+                $vet2[2] += 1;
+            } else {
+                $vet2[3] += 1;
+            }
+            $vet3[1] = $numeroCasuale;
+        }
+
 
 
 
         $stringa = implode(',', $vet);
-        $stringa2 = implode(',', $vet2);
+        $stringa2 = implode(',', $vet2);//quasi tutte le variabili chiamate stringa nel codice sono completamente inutili visto che si può stampare direttamente il valore della variabile
+        $stringa4 = implode(',', $vet3);
 
-
+        //si inizia la creazione della tabella che viene rifatta da zero tutte le volte che si pesca una carta
         $cose = "    <table border='1'>
-    <tr> <td>vincitore</td>";
-
+    <tr>";
+        $cose .= "<td>vincitore</td>";
         for ($i = 0; $i < 4; $i++) {
             if ($vet2[$i] == 3) {
                 if ($i == 0) {
-                    $vittoria = "fiori";
-                    $cose .= "<td>fiori</td>";
+                    $vittoria = "fiori";//questa è la creazione della prima riga qundi è anche dove si aggiorna la variabile vittoria
+                    $cose .= "<td><img src='IMG/RE1.gif' /></td>";
                 } else if ($i == 1) {
                     $vittoria = "quadri";
-                    $cose .= "<td>quadri</td>";
+                    $cose .= "<td><img src='IMG/RE2.gif' /></td>";
                 } else if ($i == 2) {
-                    $cose .= "<td>cuori</td>";
+                    $cose .= "<td><img src='IMG/RE3.gif' /></td>";
                     $vittoria = "cuori";
                 } else if ($i == 3) {
                     $vittoria = "picche";
-                    $cose .= "<td>picche</td>";
+                    $cose .= "<td><img src='IMG/RE4.gif' /></td>";
                 }
             } else {
                 $cose .= "<td></td>";
@@ -106,21 +170,21 @@
         }
 
         $cose .= "</tr> <tr>";
-
+        $cose .= "<td><img src='IMG/dorso.JPG' /></td>";
         for ($i = 0; $i < 4; $i++) {
-            if ($vet2[$i] == 2) {
+            if ($vet2[$i] == 2) {//creazione della secona riga
                 if ($i == 0) {
 
-                    $cose .= "<td>fiori</td>";
+                    $cose .= "<td><img src='IMG/RE1.gif' /></td>";
                 } else if ($i == 1) {
 
-                    $cose .= "<td>quadri</td>";
+                    $cose .= "<td><img src='IMG/RE2.gif' /></td>";
                 } else if ($i == 2) {
 
-                    $cose .= "<td>cuori</td>";
+                    $cose .= "<td><img src='IMG/RE3.gif' /></td>";
                 } else if ($i == 3) {
 
-                    $cose .= "<td>picche</td>";
+                    $cose .= "<td><img src='IMG/RE4.gif' /></td>";
                 } else {
                     $cose .= "<td></td>";
                 }
@@ -130,21 +194,26 @@
         }
 
         $cose .= "</tr> <tr>";
-
+        if ($vet3[1] != 0) {
+            $stringa5 = $vet3[1];//creazione della terza righa è se è il caso si aggiunge anche la carta accanto che è stata pescata per quando la riga si svuota
+            $cose .= "<td><img src='IMG/$stringa5.gif' /></td>";
+        } else {
+            $cose .= "<td><img src='IMG/dorso.JPG' /></td>";
+        }
         for ($i = 0; $i < 4; $i++) {
             if ($vet2[$i] == 1) {
                 if ($i == 0) {
 
-                    $cose .= "<td>fiori</td>";
+                    $cose .= "<td><img src='IMG/RE1.gif' /></td>";
                 } else if ($i == 1) {
 
-                    $cose .= "<td>quadri</td>";
+                    $cose .= "<td><img src='IMG/RE2.gif' /></td>";
                 } else if ($i == 2) {
 
-                    $cose .= "<td>cuori</td>";
+                    $cose .= "<td><img src='IMG/RE3.gif' /></td>";
                 } else if ($i == 3) {
 
-                    $cose .= "<td>picche</td>";
+                    $cose .= "<td><img src='IMG/RE4.gif' /></td>";
                 } else {
                     $cose .= "<td></td>";
                 }
@@ -154,17 +223,23 @@
         }
 
         $cose .= "</tr> <tr>";
+        if($vet3[0] != 0){
+            $stringa5 = $vet3[0]; //creazione dell'ultima riga (da dove partono i re) righa è se è il caso si aggiunge anche la carta accanto che è stata pescata per quando la riga si svuota
+            $cose .= "<td><img src='IMG/$stringa5.gif' /></td>";
+        }else{
+            $cose .= "<td><img src='IMG/dorso.JPG' /></td>";
+        }
 
         for ($i = 0; $i < 4; $i++) {
             if ($vet2[$i] == 0) {
                 if ($i == 0) {
-                    $cose .= "<td>fiori</td>";
+                    $cose .= "<td><img src='IMG/RE1.gif' /></td>";
                 } else if ($i == 1) {
-                    $cose .= "<td>quadri</td>";
+                    $cose .= "<td><img src='IMG/RE2.gif' /></td>";
                 } else if ($i == 2) {
-                    $cose .= "<td>cuori</td>";
+                    $cose .= "<td><img src='IMG/RE3.gif' /></td>";
                 } else if ($i == 3) {
-                    $cose .= "<td>picche</td>";
+                    $cose .= "<td><img src='IMG/RE4.gif' /></td>";
                 } else {
                     $cose .= "<td></td>";
                 }
@@ -172,20 +247,28 @@
                 $cose .= "<td></td>";
             }
         }
-
+        if($vuoto){//qua si decide quale carta stampare come ultima carta pescata che dipende dalle righe vuote
+            $stringa3 = $vet[count($vet) - 1];
+        }else{
+            $stringa3 = $vet[count($vet) - 2];
+        }
         $cose .= "</tr>";
-
+        //questo è il cuore del codice dove in pratica creo degli input invisibili nei quali metto tutti i valori che voglio memorizzare questo deve essere fatto perchè tutte le volte che richiamo il php tutte le variabili vengono azzerate quindi per memorizzarle siamo costretti a fare questo
         $cose .= "<form action='' id='frmRegistrazione'>
 
                     <input type='hidden' id='txtNominativo' name='txtNominativo' placeholder='Inserisci il Cognome/Nome' value='$stringa2' />
                     <input type='hidden' id='vin' name='vin' placeholder='Inserisci il Cognome/Nome' value='$vittoria' />
-<input type='hidden' id='sco' name='sco' placeholder='picche/cuori/quadri/fiori' value='$scommessa'/>
+                    <input type='hidden' id='sco' name='sco' placeholder='picche/cuori/quadri/fiori' value='$scommessa'/>
 
-                    <input type='hidden' id='evto' name='veto' placeholder='Inserisci il Cognome/Nome' value='$stringa' />
+                    <input type='hidden' id='veto' name='veto' placeholder='Inserisci il Cognome/Nome' value='$stringa' />
+                    <input type='hidden' id='bruh' name='bruh' placeholder='Inserisci il Cognome/Nome' value='$stringa4' />
 
                     <input type='button' value='pesca' id='btnConferma' />
 
             </form>
+                <p>
+                    ultima carta pescata: <img src='IMG/$stringa3.gif' />
+                </p>
                 <p>
                     hai scommesso su:$scommessa
                 </p>
@@ -194,7 +277,7 @@
                 </p>
         ";
 
-        echo ($cose);
+        echo ($cose);//cose contiene tutto il body della pagina che viene costruito nel codice
     }
 
 
